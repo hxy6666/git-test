177258589e04f0b6e9e774e87dc605f28cc0814e
hxy <1300722743@qq.com>
2022-07-01 17:45:56 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author hxy
+ * @create 2022-07-01 17:40
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
